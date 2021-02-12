894bf2575c9622f8dcd2e3d40977527aed6cc531
Arnav Gupta <arnav@codingblocks.com>
2021-02-12 14:25:10 +0530

    init
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ basics.js
@@ -0,0 +1,13 @@
+// variables
+let a = 10
+let c = "Asdas"
+const b = 20
+
+console.log(a)
+console.log(b)
+console.log(typeof a)
+console.log(typeof c)
+c = 34
+console.log(typeof c)
+
+// datatypes
