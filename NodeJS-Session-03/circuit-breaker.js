ef84bcd0169b42313626febfe7b603ffe37c8448
Arnav Gupta <arnav@codingblocks.com>
2021-02-23 15:34:56 +0530

    js event loop
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ NodeJS-Session-03/circuit-breaker.js
@@ -0,0 +1,24 @@
+
+let a = true 
+let c = 0;
+
+
+setTimeout(() => {
+  a = false;
+}, 1000)
+
+
+// while (a) {
+//   console.log(++c);
+// }
+
+function z () {
+
+}
+
+function y () {
+  z()
+}
+function x () {
+ y()
+}
